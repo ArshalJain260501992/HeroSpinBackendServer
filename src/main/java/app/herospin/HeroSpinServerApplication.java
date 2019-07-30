3617644ac37fc19872961e61413d0786d8097c33
Arshal Jain <akjp26536@gmail.com>
2019-07-30 08:40:28 +0400

    minor code cleanup changes

--- src/main/java/app/herospin/HeroSpinServerApplication.java
+++ src/main/java/app/herospin/HeroSpinServerApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.slf4j.Slf4j;
@@ -25,19 +24,5 @@ public class HeroSpinServerApplication {
 		log.info("Rest template genrated");
 		return builder.build();
 	}
-
-//	@Bean
-//	JedisConnectionFactory jedisConnectionFactory() {
-//		log.info("Jedis connection factory generated");
-//		return new JedisConnectionFactory();
-//	}
-//
-//	@Bean
-//	public RedisTemplate<String, Object> redisTemplate() {
-//		RedisTemplate<String, Object> template = new RedisTemplate<>();
-//		template.setConnectionFactory(jedisConnectionFactory());
-//		log.info("Redis template generated");
-//		return template;
-//	}
 
 }
